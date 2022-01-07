6bc74288924dc23b950f732d3cb3d1bf3f148d38
Atharva Wagh <gameboy369065@gmail.com>
2022-01-08 00:40:37 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKamn3-7ATEhz4kl9v3TtqI5_K0LKLYUc",
+    authDomain: "clone-86078.firebaseapp.com",
+    projectId: "clone-86078",
+    storageBucket: "clone-86078.appspot.com",
+    messagingSenderId: "890417532756",
+    appId: "1:890417532756:web:d23f8521ab62f6c42282c1"
+  };
